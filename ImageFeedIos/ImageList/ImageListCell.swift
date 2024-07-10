ef662c8c9a5880ea7b1c8ac6edaa3f2ecfa79450
vl ad <8.kchayyy@gmail.com>
2024-07-10 22:13:40 +0200

    Создана лента изображений

--- ImageFeedIos/ImageList/ImageListCell.swift
+++ ImageFeedIos/ImageList/ImageListCell.swift
@@ -1,5 +1,10 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+   
+    @IBOutlet weak var imagePhotoView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     static let reuseIdentifier = "ImagesListCell"
 }
--- ImageFeedIos/ImageList/ImagesListViewController.swift
+++ ImageFeedIos/ImageList/ImagesListViewController.swift
@@ -2,36 +2,83 @@ import UIKit
 
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
-
+    
+    private let photosName: [String] = Array(0..<20).map{"\($0)"}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.rowHeight = 200
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        let index = photosName.count
+        return index
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-
+        
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-
-        configCell(for: imageListCell)
-
+        
+        configCell(for: imageListCell, with: indexPath)
+        
         return imageListCell
     }
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell) { }
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        //индекс ячейки
+        let index = indexPath.row
+        
+        //получаем имя изображения по индексу
+        let imageName = photosName[index]
+        
+        //проверка на наличие нужного изображения
+        guard let image = UIImage(named: imageName) else {
+            return
+        }
+        
+        cell.imagePhotoView.image = image
+        
+        var dateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            formatter.timeStyle = .none
+            return formatter
+        } ()
+        
+        let currentDate = dateFormatter.string(from: Date())
+        cell.dateLabel.text = currentDate
+        
+        if index % 2 != 0 {
+            cell.likeButton.setImage(UIImage(named: "LikeButtonOn"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "LikeButtonOff"), for: .normal)
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
 }
