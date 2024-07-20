d05224a4dd9f8ec897e52c60c3446c40381ff941
vl ad <8.kchayyy@gmail.com>
2024-07-20 14:11:16 +0200

    Экран профиль сверстан

---
+++ ImageFeedIos/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var userPic: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userTag: UILabel!
+    @IBOutlet weak var userDescription: UILabel!
+    @IBOutlet weak var exitButton: UIButton!
+    
+    
+    @IBAction func exitButtonDidTap(_ sender: Any) {
+    }
+}
