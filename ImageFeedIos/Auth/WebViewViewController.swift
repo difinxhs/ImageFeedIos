8d40c97da713a0a79535a2f9d3dbb4de6ffd59a9
vl ad <8.kchayyy@gmail.com>
2024-07-30 21:55:43 +0200

    WKNavigationDelegate

--- ImageFeedIos/Auth/WebViewViewController.swift
+++ ImageFeedIos/Auth/WebViewViewController.swift
@@ -10,12 +10,14 @@ final class WebViewViewController: UIViewController {
     
     override func viewDidLoad() {
         loadAuthView()
+        webView.navigationDelegate = self
     }
     
     @IBAction func backButtonDidTap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    //Запрос на сервер с аутентификацией
     private func loadAuthView() {
         guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
             return
@@ -35,4 +37,33 @@ final class WebViewViewController: UIViewController {
         let request = URLRequest(url: url)
         webView.load(request)
         }
+}
+//MARK: WKNavigationDelegate
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            //TODO: process code
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: {$0.name == "code"})
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
 }
