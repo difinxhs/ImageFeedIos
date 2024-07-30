bc8c97dc2aa3f052cc58d2e2586e18c91af3245e
vl ad <8.kchayyy@gmail.com>
2024-07-30 20:19:14 +0200

    кастомная кнопка назад

--- ImageFeedIos/Auth/AuthViewController.swift
+++ ImageFeedIos/Auth/AuthViewController.swift
@@ -1,6 +1,17 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
-    //private let showWebViewSegueIdentifier = "ShowWebView"
+    private let showWebViewSegueIdentifier = "ShowWebView"
     @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        configureBackButton()
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "ypBlack")
+    }
 }
