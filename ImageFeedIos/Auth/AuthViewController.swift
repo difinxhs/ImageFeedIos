da08a1a96f6c6485dfc10f54413fcedcac7f6e78
vl ad <8.kchayyy@gmail.com>
2024-08-13 23:01:21 +0200

    добавил ProgressHUD и индикатор загрузки

--- ImageFeedIos/Auth/AuthViewController.swift
+++ ImageFeedIos/Auth/AuthViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 protocol AuthViewControllerDelegate: AnyObject {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String)
@@ -26,9 +27,13 @@ extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         
         vc.dismiss(animated: true)
+        ProgressHUD.animate()
         
         OAuth2Service.shared.fetchOAuthToken(code: code) { [weak self] result in
             guard let self = self else { return }
+            
+            ProgressHUD.dismiss()
+            
             switch result {
             case .success(let token):
                 print("Successfully fetched OAuth token: \(token)")
