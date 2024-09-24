7cf8b8d4527780e8a3e9882c8b9336308fac782a
vl ad <8.kchayyy@gmail.com>
2024-09-25 01:23:01 +0200

    первый тест сделан и работает

---
+++ ImageFeedIosTests/WebViewTests.swift
@@ -0,0 +1,20 @@
+@testable import ImageFeedIos
+import XCTest
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+            
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled) //behaviour verification
+    }
+}
+
