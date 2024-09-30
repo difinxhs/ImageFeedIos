1f4d55b38bd9f5036b4d61a3591d1b86c7f54266
vl ad <8.kchayyy@gmail.com>
2024-09-30 22:04:35 +0200

    написан testAuth

--- ImageFeedIosUITests/ImageFeedIosUITests.swift
+++ ImageFeedIosUITests/ImageFeedIosUITests.swift
@@ -10,7 +10,38 @@ class Image_FeedUITests: XCTestCase {
     }
     
     func testAuth() throws {
-        // тестируем сценарий авторизации
+        app.buttons["Authenticate"].tap()
+        
+        let webView = app.webViews["UnsplashWebView"]
+        
+        XCTAssertTrue(webView.waitForExistence(timeout: 5))
+
+        let loginTextField = webView.descendants(matching: .textField).element
+        XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
+        
+        loginTextField.tap()
+        loginTextField.typeText("8.kchayyy@gmail.com")
+        let doneButton = app.buttons["Done"] // Локализованная версия может отличаться
+        if doneButton.exists {
+            doneButton.tap()
+        }
+        
+        let passwordTextField = webView.descendants(matching: .secureTextField).element
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
+        
+        passwordTextField.tap()
+        sleep(1)
+        passwordTextField.typeText("Empire7511")
+        if doneButton.exists {
+            doneButton.tap()
+        }
+        
+        webView.buttons["Login"].tap()
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
     
     func testFeed() throws {
