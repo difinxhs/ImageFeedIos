ca1c35277cea3b18d8b2b4f6f9b7bf97b644f8b3
vl ad <8.kchayyy@gmail.com>
2024-09-30 20:52:39 +0200

    подготовка файла к тестам

--- ImageFeedIosUITests/ImageFeedIosUITests.swift
+++ ImageFeedIosUITests/ImageFeedIosUITests.swift
@@ -1,36 +1,23 @@
 import XCTest
 
-final class ImageFeedIosUITests: XCTestCase {
-
+class Image_FeedUITests: XCTestCase {
+    private let app = XCUIApplication() // переменная приложения
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = false // настройка выполнения тестов, которая прекратит выполнения тестов, если в тесте что-то пошло не так
+        
+        app.launch() // запускаем приложение перед каждым тестом
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAuth() throws {
+        // тестируем сценарий авторизации
     }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testFeed() throws {
+        // тестируем сценарий ленты
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testProfile() throws {
+        // тестируем сценарий профиля
     }
 }
